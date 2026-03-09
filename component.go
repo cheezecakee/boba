c5062e18af690bb6d026392cf3c184bd40638a48
cheezecakee <anabel.pmoraes@gmail.com>
2026-03-09 14:20:20 -0300

    feat: New component interface

---
+++ component.go
@@ -0,0 +1,22 @@
+package boba
+
+// Component is the protocol between Block and boba component.
+// All predefined boba component implement this interface.
+type Component interface {
+	SetItems(Items)
+}
+
+// ItemsMsg is sent by Block to push items into a component after init.
+type ItemsMsg struct {
+	Items Items
+}
+
+// SelectedItemMsg is sent by a component to notify Block of the current selection.
+type SelectedItemMsg struct {
+	Item Item
+}
+
+// CursorMsg is sent by a component to sync its internal cursor position with Block.
+type CursorMsg struct {
+	Cursor Cursor
+}
